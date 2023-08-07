28ed658c982d63d044f5131a5df81e34aeed1cfa
PGETyler <brownt2357@hotmail.com>
2023-08-07 12:01:15 -0400

    Add project files.

---
+++ HelloWorldTemplate.cpp
@@ -0,0 +1,12 @@
+// HelloWorldTemplate.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Put your name here
+// put the class name here
+// put the assignment name here
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
